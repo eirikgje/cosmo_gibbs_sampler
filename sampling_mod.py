2086ab4a2c9afa9650a1abd153f13f7ebca5c490
Eirik Gjerlow <egjerlow@gmail.com>
2014-03-16 00:09:40 +0100

    Now the sampler works and one can gibbs sample (at least using T+E)

--- sampling_mod.py
+++ sampling_mod.py
@@ -6,24 +6,24 @@ import utils
 #Remember that everything is in non-l(l+1) normalization
 
 def sample_constrained_realization(currcls, params):
-    constrained_realization = np.zeros((utils.get_num_modes(2, params.lmax+1), np.shape(params.data)[1]))
-    for l in range(2, params.lmax+1):
+    constrained_realization = np.zeros((utils.get_num_modes(0, params.lmax), np.shape(params.data)[1]))
+    for l in range(3, params.lmax+1):
         covmat = np.linalg.inv(np.linalg.inv(currcls[l, :, :]) + params.invnoise[l, :, :])
         mean = params.data[utils.l2mrange(l), :].copy()
-        mean = np.dot(np.dot(covmat, params.invnoise), mean[:, :, np.newaxis])[:, :, 0].T
-        constrained_realization[utils.l2mrange(l)] = np.random.multivariate_normal(np.zeros(mean.shape[1]), covmat, size=mean.shape[0])
-        constrained_realization[utils.l2mrange(l)] += mean
+        mean = np.dot(np.dot(covmat, params.invnoise[l, :, :]), mean[:, :, np.newaxis])[:, :, 0].T
+        constrained_realization[utils.l2mrange(l), :] = np.random.multivariate_normal(np.zeros(mean.shape[1]), covmat, size=mean.shape[0])
+        constrained_realization[utils.l2mrange(l), :] += mean
     return constrained_realization
 
 def sample_cls(currsignal, params, prior=None):
     #This depends on the prior. None corresponds to 'flat' prior (though that is not well-defined)
-    cls = np.zeros(params.lmax+1)
+    cls = np.zeros((params.lmax+1, currsignal.shape[1], currsignal.shape[1]))
 
-    for l in range(2, params.lmax+1):
+    for l in range(3, params.lmax+1):
         if prior is None and params.pol:
             #THIS ONLY HOLDS IF dim(cls) = 2 - i.e. no B modes. Otherwise, df = 2l - 3
             df = 2 * l - 2
-            cls[l, :, :] = sample_inv_wishart(currsignal[utils.l2mrange(l), :], df, l)
+            cls[l, :, :] = sample_inv_wishart(currsignal[utils.l2mrange(l), :], df)
         if prior is None and not params.pol:
             alpha = (2 * l - 1) / 2
             cls[l, :, :] = sample_inv_gamma(currsignal[utils.l2mrange(l), :], alpha)
@@ -34,12 +34,16 @@ def sample_cls(currsignal, params, prior=None):
 def sample_inv_wishart(alms, df):
     #These are really sigma*2l+1, but in the inverse wishart and inverse gamma
     #sampling formalisms, sigma*2l+1 is the 'scaling matrix'
-    sigma = np.sum(alms**2, 0)
+#    sigma = np.sum(alms**2, 0)
+    sigma = np.zeros((alms.shape[1], alms.shape[1]))
+    for i in xrange(alms.shape[1]):
+        for j in xrange(alms.shape[1]):
+            sigma[i, j] = np.sum(alms[:, i]*alms[:, j])
     invsigma = np.linalg.inv(sigma)
 
     mean = np.zeros(2)
 
-    samps = np.random.multivariate_normal(mean, invsigma, nu)
+    samps = np.random.multivariate_normal(mean, invsigma, df)
     res = np.zeros((sigma.shape))
     for i in range(df):
         res += np.outer(samps[i,:], samps[i, :])
@@ -53,3 +57,15 @@ def sample_inv_gamma(alms, alpha):
 #Corresponds to the wikipedia notation
 def sample_gamma(alpha, beta):
     return np.random.gamma(alpha, 1/beta)
+
+def sample_alms(cls):
+    alms = np.zeros((utils.get_num_modes(0, cls.shape[0]-1), cls.shape[1]))
+    mean = np.zeros(cls.shape[1])
+    for l in range(3, cls.shape[0]):
+        alms[utils.l2mrange(l), :] = np.random.multivariate_normal(mean, cls[l], size=2*l+1)
+    return alms
+
+def sample_white_noise(val, lmax, dim):
+    mean = np.zeros(dim)
+    return np.random.multivariate_normal(mean, val, size=utils.get_num_modes(0, lmax))
+
--- utils.py
+++ utils.py
@@ -1,29 +1,86 @@
 from __future__ import division
 
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy.special import gamma
 
-def get_noise_powerspec(rms, lmax=47, nside=16, beam_fwhm=None, pol=False):
-    "FWHM in arcmin"
-    l = np.arange(0, lmax + 1)
-    npix = 12 * nside ** 2
-    spec = 4 * np.pi / npix * rms ** 2 * l * (l + 1) / (2 * np.pi)
-    if beam_fwhm is not None:
-        sigma = beam_fwhm/(60.0 * 180.0) * np.pi / np.sqrt(8.0*np.log(2.0))
-        sig2 = sigma ** 2
-        g = np.exp(-0.5*l*(l+1.0)*sig2)
-        if pol:
-            factor_pol = np.exp([0.0, 2.0*sig2, 2.0*sig2])
-            gout = g * np.exp(2.0 *sig2)
-        else:
-            gout = g
-        spec = spec / g**2
-    return spec
+#def get_noise_powerspec(rms, lmax=47, nside=16, beam_fwhm=None, pol=False):
+#    "FWHM in arcmin"
+#    l = np.arange(0, lmax + 1)
+#    npix = 12 * nside ** 2
+#    spec = 4 * np.pi / npix * rms ** 2 * l * (l + 1) / (2 * np.pi)
+#    spec = 4 * np.pi / npix * rms ** 2
+#    if beam_fwhm is not None:
+#        sigma = beam_fwhm/(60.0 * 180.0) * np.pi / np.sqrt(8.0*np.log(2.0))
+#        sig2 = sigma ** 2
+#        g = np.exp(-0.5*l*(l+1.0)*sig2)
+#        if pol:
+#            factor_pol = np.exp([0.0, 2.0*sig2, 2.0*sig2])
+#            gout = g * np.exp(2.0 *sig2)
+#        else:
+#            gout = g
+#        spec = spec / g**2
+#    return spec
+def get_noise_powerspec(lmax, value, dim):
+    noise = np.zeros((lmax+1, dim, dim))
+    noise[:, :, :] = value[np.newaxis, :, :]
+    return noise
 
 def l2mrange(l):
-    start = l**2 - (l-1)**2 - 3
-    end = start + (l+1)**2 - l ** 2
+    start = 0
+    start = get_num_modes(0, l-1)
+    end = start + get_num_modes(l, l)
+#    for currl in xrange(l):
+#        start += get_num_modes(currl, currl)
+#    end = start + get_num_modes(l, l)
     return slice(start, end)
 
 def get_num_modes(lmin, lmax):
     #Find number of modes between lmin and lmax, inclusive
     return (lmax+1) ** 2 - lmin**2
+
+def cls2matrix(cl, ordering, dim):
+    s = np.zeros((dim, dim))
+    s[0, 0] = cl[ordering['TT']]
+    if dim == 1:
+        return s
+    s[1, 0] = cl[ordering['TE']]
+    s[0, 1] = s[1, 0]
+    s[1, 1] = cl[ordering['EE']]
+    if dim == 2:
+        return s
+    s[2, 0] = cl[ordering['TB']]
+    s[0, 2] = s[2, 0]
+    s[2, 1] = cl[ordering['EB']]
+    s[1, 2] = s[2, 1]
+    s[2, 2] = cl[ordering['BB']]
+    return s
+
+def cls2matrix_all(cl, ordering, dim):
+    s = np.zeros((cl.shape[0], dim, dim))
+    for l in xrange(cl.shape[0]):
+        s[l, :, :] = cls2matrix(cl[l, :], ordering, dim)
+    return s
+
+def alms2sigma(alms):
+    lmax = int(np.sqrt(alms.shape[0]) - 1)
+    sigma = np.zeros((lmax+1, alms.shape[1], alms.shape[1]))
+    for l in range(lmax+1):
+        for i in range(alms.shape[1]):
+            for j in range(alms.shape[1]):
+                sigma[l, i, j] = np.sum(alms[l2mrange(l), i]*alms[l2mrange(l), j]) / (2 * l + 1)
+    return sigma
+
+def convert_normalization(cls, lmax, mode='normalize'):
+    ls = np.arange(lmax+1)
+    #not used anyway
+    ls[0] = 1.0
+    if mode == 'normalize':
+        factor = ls * (ls+1) / (2 * np.pi)
+    elif mode == 'unnormalize':
+        factor = 2 * np.pi / (ls * (ls + 1))
+
+    return cls*factor[:, np.newaxis, np.newaxis]
+
+def get_inverse_wishart_1d(newnu, scale, x):
+    return scale**(newnu / 2) / (2 **(newnu / 2)) / gamma(newnu/2) / x ** ((newnu + 2) / 2) * np.exp(-0.5 * scale / x)
